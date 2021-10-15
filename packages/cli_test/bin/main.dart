ef9d5d3b3abaad24f9769e0eef6e82d07c73ace9
Remi Rousselet <darky12s@gmail.com>
2021-10-15 18:28:06 +0100

    remove throwing code

--- packages/cli_test/bin/main.dart
+++ packages/cli_test/bin/main.dart
@@ -4,10 +4,7 @@ void main() {
   print('''
 my_test:
 hasTerminal: ${stdout.hasTerminal}
-nonBlocking: ${stdout.nonBlocking}
 supportsAnsiEscapes: ${stdout.supportsAnsiEscapes}
-terminalColumns: ${stdout.terminalColumns}
-terminalLines: ${stdout.terminalLines}
 encoding: ${stdout.encoding}
 ''');
 }
