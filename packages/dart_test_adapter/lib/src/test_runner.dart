3c053467b9ecaf54f54472c8fc818c86866b6f25
Remi Rousselet <darky12s@gmail.com>
2021-12-30 09:30:48 +0100

    Remove dead code

--- packages/dart_test_adapter/lib/src/test_runner.dart
+++ packages/dart_test_adapter/lib/src/test_runner.dart
@@ -4,12 +4,6 @@ import 'dart:io';
 
 import 'test_protocol.dart';
 
-enum WebRenderer {
-  auto,
-  canvaskit,
-  html,
-}
-
 Stream<List<TestEvent>> flutterTest({
   Map<String, String>? environment,
   List<String>? arguments,
@@ -94,72 +88,3 @@ Stream<List<TestEvent>> _parseTestJsonOutput(
 
   return controller.stream;
 }
-
-// /// Wraps a Stream to allow accessing elements previously emitted and
-// /// yet-to-be-emitted events at the same time.
-// ///
-// /// This is useful for things such as finding a specific event, which could
-// /// already be emitted, but may not be yet.
-// class _ReactiveWrapper<Event> {
-//   _ReactiveWrapper(Stream<Event> stream)
-//       : _stream = stream.asBroadcastStream() {
-//     _subscription = _stream.listen(_events.add);
-//   }
-
-//   final _events = <Event>[];
-//   final Stream<Event> _stream;
-//   late final StreamSubscription<Event> _subscription;
-
-//   FutureOr<T> firstWhereIs<T>() {
-//     final value = _events.firstWhereOrNull((e) => e is T) as T?;
-
-//     if (value != null) return value;
-
-//     return _stream.firstWhere((e) => e is T).then((e) => e as T);
-//   }
-
-//   Stream<T> whereIs<T>() async* {
-//     final events = _events.whereType<T>().toList();
-//     for (final event in events) yield event;
-
-//     yield* _stream.where((e) => e is T).cast<T>();
-//   }
-
-//   Future<void> close() {
-//     return _subscription.cancel();
-//   }
-// }
-
-// class TestResult {
-//   TestResult._(this._processFuture, this._events);
-
-//   final Future<Process> _processFuture;
-//   final _ReactiveWrapper<TestEvent> _events;
-
-//   Stream<TestEvent> all() => _events.whereIs<TestEvent>();
-
-//   FutureOr<TestEventStart> start() => _events.firstWhereIs<TestEventStart>();
-//   FutureOr<TestEventDone> done() => _events.firstWhereIs<TestEventDone>();
-
-//   Future<TestEventAllSuites> allSuites() =>
-//       _events.whereIs<TestEventAllSuites>().first;
-
-//   Stream<TestEventSuite> suites() => _events.whereIs<TestEventSuite>();
-
-//   Stream<TestEventGroup> groups() => _events.whereIs<TestEventGroup>();
-
-//   Stream<TestEventMessage> messages() => _events.whereIs<TestEventMessage>();
-
-//   Stream<TestEventTestStart> testStart() =>
-//       _events.whereIs<TestEventTestStart>();
-
-//   Stream<TestEventTestDone> testDone() => _events.whereIs<TestEventTestDone>();
-
-//   Stream<TestEventTestError> testError() =>
-//       _events.whereIs<TestEventTestError>();
-
-//   Future<void> dispose() async {
-//     _events.close();
-//     (await _processFuture).kill();
-//   }
-// }
